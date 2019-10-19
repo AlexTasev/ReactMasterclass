47ff20e6ee6c70261488552a3b444ee081a5e6e1
AlexTasev <aleksandar_tasev@abv.bg>
2019-10-19 18:24:26 +0300

    feat: authorized-routes implemented

--- react-masterclass/src/App.jsx
+++ react-masterclass/src/App.jsx
@@ -1,42 +1,43 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router,
+  Redirect
+} from "react-router-dom";
+import "./App.css";
 
-import Login from './pages/login/Login';
-import Episodes from './pages/episodes/Episodes';
-import Characters from './pages/characters/Characters';
-import Starships from './pages/starships/Starships';
-import Navbar from './components/common/Navbar';
+import Login from "./pages/login/Login";
+import Episodes from "./pages/episodes/Episodes";
+import Characters from "./pages/characters/Characters";
+import Starships from "./pages/starships/Starships";
+import Navbar from "./components/common/Navbar";
 
 const App = () => {
-  let isLoggedIn = false;
+  let isLoggedIn = true;
 
   return (
     <Router>
+      {isLoggedIn && <Navbar />}
       <Switch>
         <Route exact path="/">
           {isLoggedIn ? <Redirect to="/episodes" /> : <Redirect to="/login" />}
         </Route>
-        <Route
-          path="/login"
-          component={Login}
-        />
-        <Navbar/>
-        <Route
-          path="/episodes"
-          component={Episodes}
-        />
-        <Route
-          path="/characters"
-          component={Characters}
-        />
-        <Route
-          path="/starships"
-          component={Starships}
-        />
+        {isLoggedIn ? (
+          <>
+            <Route path="/episodes" component={Episodes} />
+            <Route path="/characters" component={Characters} />
+            <Route path="/starships" component={Starships} />
+          </>
+        ) : (
+          <>
+            <Redirect to="/login" />
+            <Route path="/login" component={Login} />
+          </>
+        )}
       </Switch>
     </Router>
   );
-}
+};
 
 export default App;
--- react-masterclass/src/pages/episodes/Episode-item.jsx
+++ react-masterclass/src/pages/episodes/Episode-item.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const EpizodeItem = () => {
-    return (
-        <section>
-            <img></img>
-            <div className="title"></div>
-            <div className="descrtiption"></div>
-        </section>
-    );
+  return (
+    <section>
+      <img></img>
+      <div className="title">Some Episode</div>
+      <div className="descrtiption">
+        Some Random descrtiption to check if it works Some Random descrtiption
+        to check if it works Some Random descrtiption to check if it works Some
+        Random descrtiption to check if it works Some Random descrtiption to
+        check if it works Some Random descrtiption to check if it works Some
+        Random descrtiption to check if it works Some Random descrtiption to
+        check if it works Some Random descrtiption to check if it works Some
+        Random descrtiption to check if it works Some Random descrtiption to
+        check if it works Some Random descrtiption to check if it works Some
+        Random descrtiption to check if it works Some Random descrtiption to
+        check if it works Some Random descrtiption to check if it works Some
+        Random descrtiption to check if it works Some Random descrtiption to
+        check if it works Some Random descrtiption to check if it works Some
+        Random descrtiption to check if it works Some Random descrtiption to
+        check if it works Some Random descrtiption to check if it works Some
+        Random descrtiption to check if it works Some Random descrtiption to
+        check if it works Some Random descrtiption to check if it works
+      </div>
+    </section>
+  );
 };
 
 export default EpizodeItem;
