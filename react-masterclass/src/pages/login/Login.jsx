8e2305db4eede5a3a409693db387f9c0235674d9
AlexTasev <aleksandar_tasev@abv.bg>
2019-11-03 17:11:42 +0200

    feat: sign in works, auth token is available in ls

--- react-masterclass/src/pages/login/Login.jsx
+++ react-masterclass/src/pages/login/Login.jsx
@@ -1,19 +1,44 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
+import gql from "graphql-tag";
+import { useApolloClient, useMutation } from "@apollo/react-hooks";
 import Logo from "../../components/common/Logo";
 
-const Login = ({ signInUser }) => {
+const SIGN_IN = gql`
+  mutation signIn($email: String!, $password: String!) {
+    signIn(email: $email, password: $password) {
+      token
+    }
+  }
+`;
+
+const Login = (props) => {
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
 
   let credentialsError = false;
 
+  const client = useApolloClient();
+
+  const [login, { loading, error }] = useMutation(SIGN_IN, {
+    onCompleted: ({ signIn: token }) => {
+      localStorage.setItem("token", token.token);
+      client.writeData({ data: { authenticated: true } });
+      props.history.push("/episodes");  
+    }
+  });
+
+  if (loading) return <span>Loading...</span>;
+  if (error) return <p>An error occurred</p>;
+
+  if (localStorage.getItem("token"))
+    return <Redirect to="/episodes"></Redirect>;
+
   const handleSubmit = event => {
     event.preventDefault();
 
     if (userEmail && userPassword) {
-      const user = { email: userEmail, password: userPassword };
-      signInUser(user);
+      login({ variables: { email: userEmail, password: userPassword } });
     } else {
       credentialsError = true;
     }
